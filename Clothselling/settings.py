73c33f035c06926f20ee9ea9e24e76290a53a38c
AbhinandNtk <abhinadntkm@gamail.com>
2023-10-06 17:57:43 +0530

    Setting files changed

--- Clothselling/settings.py
+++ Clothselling/settings.py
@@ -142,13 +142,15 @@ AUTH_USER_MODEL = "admin_auth.CustomUser"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'     
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Clothselling/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # media files configurations
 
 MEDIA_URL='/media/'
-MEDIA_ROOT =BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_HOST = 'smtp.gmail.com'
