0faf3a37411fe0403b6d2fdcae695d09b25e91f4
AbhinandNtk <abhinadntkm@gamail.com>
2023-10-10 20:22:32 +0530

    Added address validation in all the places

--- Clothselling/static/js/validationaddress.js
+++ Clothselling/static/js/validationaddress.js
@@ -1,5 +1,87 @@
 const phoneRegex = /^\d{10}$/;
 const pincodeRegex = /^\d{6}$/;
+const nameregex = /^(?=.*[A-Za-z])[A-Za-z\s]+$/;
+
+
+document.getElementById("firstname").addEventListener("input", function () {
+    const firstname = this.value.trim(); // Remove leading and trailing spaces
+
+    if (nameregex.test(firstname)) {
+        document.getElementById("validationMessageforfirstname").textContent = " ";
+        document.getElementById("validationMessageforfirstname").style.color = 'green';
+    } else {
+        document.getElementById("validationMessageforfirstname").textContent = "Please Enter a  valid Name";
+        document.getElementById("validationMessageforfirstname").style.color = 'red';
+    }
+});
+
+
+document.getElementById("lastname").addEventListener("input", function () {
+    const lastname = this.value.trim(); // Remove leading and trailing spaces
+
+    if (nameregex.test(lastname)) {
+        document.getElementById("validationMessageforlastname").textContent = " ";
+        document.getElementById("validationMessageforlastname").style.color = 'green';
+    } else {
+        document.getElementById("validationMessageforlastname").textContent = "Please Enter a  valid LastName";
+        document.getElementById("validationMessageforlastname").style.color = 'red';
+    }
+});
+
+
+document.getElementById("address").addEventListener("input", function () {
+    const address = this.value.trim(); // Remove leading and trailing spaces
+
+    if (nameregex.test(address)) {
+        document.getElementById("validationMessageforaddress").textContent = " ";
+        document.getElementById("validationMessageforaddress").style.color = 'green';
+    } else {
+        document.getElementById("validationMessageforaddress").textContent = "Please Enter a  valid Address";
+        document.getElementById("validationMessageforaddress").style.color = 'red';
+    }
+});
+
+document.getElementById("town").addEventListener("input", function () {
+    const town = this.value.trim(); // Remove leading and trailing spaces
+
+    if (nameregex.test(town)) {
+        document.getElementById("validationMessagefortown").textContent = " ";
+        document.getElementById("validationMessagefortown").style.color = 'green';
+    } else {
+        document.getElementById("validationMessagefortown").textContent = "Please Enter a  valid Town";
+        document.getElementById("validationMessagefortown").style.color = 'red';
+    }
+});
+
+document.getElementById("nearbylocation").addEventListener("input", function () {
+    const nearbylocation = this.value.trim(); // Remove leading and trailing spaces
+
+    if (nameregex.test(nearbylocation)) {
+        document.getElementById("validationMessagefornearbylocation").textContent = " ";
+        document.getElementById("validationMessagefornearbylocation").style.color = 'green';
+    } else {
+        document.getElementById("validationMessagefornearbylocation").textContent = "Please Enter a  valid nearbylocation";
+        document.getElementById("validationMessagefornearbylocation").style.color = 'red';
+    }
+});
+
+
+document.getElementById("district").addEventListener("input", function () {
+    const district = this.value.trim(); // Remove leading and trailing spaces
+
+    if (nameregex.test(district)) {
+        document.getElementById("validationMessagefordistrict").textContent = " ";
+        document.getElementById("validationMessagefordistrict").style.color = 'green';
+    } else {
+        document.getElementById("validationMessagefordistrict").textContent = "Please Enter a  valid District";
+        document.getElementById("validationMessagefordistrict").style.color = 'red';
+    }
+});
+
+
+
+
+
 
 document.getElementById("phonenumber").addEventListener("input", function () {
     const phonenumber = this.value.trim(); // Remove leading and trailing spaces
@@ -30,8 +112,9 @@ document.getElementById("addressform").addEventListener("submit", function (even
 
     const phonenumber = document.getElementById("phonenumber").value.trim(); // Remove leading and trailing spaces
     const zipcode = document.getElementById("zipcode").value.trim(); // Remove leading and trailing spaces
+    const firstname = document.getElementById("firstname").value.trim(); // Remove leading and trailing spaces
 
-    if (phoneRegex.test(phonenumber) && pincodeRegex.test(zipcode)) {
+    if (phoneRegex.test(phonenumber) && pincodeRegex.test(zipcode) && nameregex.test(firstname))   {
         document.getElementById("validationMessage").textContent = "Form submitted successfully!";
         document.getElementById("addressform").submit();
     } else {
